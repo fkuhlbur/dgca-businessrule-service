6af24c7dcbfb9b0630585485539b6a8e21f16eef
m.schulte <M.Schulte@t-systems.com>
2021-11-04 14:25:57 +0100

    added logging filter

--- src/main/java/eu/europa/ec/dgc/businessrule/config/WebSecurityConfig.java
+++ src/main/java/eu/europa/ec/dgc/businessrule/config/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package eu.europa.ec.dgc.businessrule.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -10,6 +12,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
+    }
+
+    /**
+     * Logging filter to debug requerst headers.
+     * @return logging filter to debug requerst headers.
+     */
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setIncludeHeaders(true);
+        return loggingFilter;
     }
 
 }
